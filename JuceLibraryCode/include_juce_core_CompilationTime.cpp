02ff3159265f73ba7515bff8f59aed7fa3566ca3
benzomixedit <bennymixedit@gmail.com>
2025-08-18 23:28:33 -0400

    Windows: define juce_compilationDate/Time as arrays to satisfy MSVC linker

--- JuceLibraryCode/include_juce_core_CompilationTime.cpp
+++ JuceLibraryCode/include_juce_core_CompilationTime.cpp
@@ -28,7 +28,7 @@
 namespace juce
 {
 
-extern const char* const juce_compilationDate = __DATE__;
-extern const char* const juce_compilationTime = __TIME__;
+const char juce_compilationDate[] = __DATE__;
+const char juce_compilationTime[] = __TIME__;
 
 } // namespace juce
